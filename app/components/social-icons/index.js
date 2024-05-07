cc2cce14419ccdc813c176eccb53431f2cc48d0f
常德恩 <changdeen@sailvan.com>
2024-05-07 18:02:58 +0800

    ✨ feat: tailwind plugin

--- app/components/social-icons/index.js
+++ app/components/social-icons/index.js
@@ -1,41 +1,43 @@
+import clsx from "clsx";
 import {
-    Facebook,
-    Github,
-    Linkedin,
-    Mail,
-    Twitter,
-    Youtube,
-    Mastodon,
-    Threads,
-    Instagram,
-} from './icons'
+  Facebook,
+  Github,
+  Linkedin,
+  Mail,
+  Twitter,
+  Youtube,
+  Mastodon,
+  Threads,
+  Instagram,
+} from "./icons";
 
 const components = {
-    mail: Mail,
-    github: Github,
-    facebook: Facebook,
-    youtube: Youtube,
-    linkedin: Linkedin,
-    twitter: Twitter,
-    mastodon: Mastodon,
-    threads: Threads,
-    instagram: Instagram,
-}
+  mail: Mail,
+  github: Github,
+  facebook: Facebook,
+  youtube: Youtube,
+  linkedin: Linkedin,
+  twitter: Twitter,
+  mastodon: Mastodon,
+  threads: Threads,
+  instagram: Instagram,
+};
 
-export default function SocialIcon({kind, href, size = 8}) {
-  
-    if (!href) return null
-    const SocialSvg = components[kind]
-    console.log(SocialSvg);
-    return (
-        <a
-            className="text-sm text-gray-500 transition hover:text-gray-600"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={href}
-        >
-            <SocialSvg className={`fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400  h-${size} w-${size}`} />
-        </a>
-
-    );
+export default function SocialIcon({ kind, href }) {
+  if (!href) return null;
+  const SocialSvg = components[kind];
+  return (
+    <a
+      className="text-sm text-gray-500 transition hover:text-gray-600"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+    >
+      <SocialSvg
+        className={
+          "fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-8 w-8"
+        }
+      />
+    </a>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,7 +14,48 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: colors.pink,
+        gray: colors.gray,
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme("colors.primary.500"),
+              "&:hover": {
+                color: `${theme("colors.primary.600")}`,
+              },
+              code: { color: theme("colors.primary.400") },
+            },
+            "h1,h2": {
+              fontWeight: "700",
+              letterSpacing: theme("letterSpacing.tight"),
+            },
+            h3: {
+              fontWeight: "600",
+            },
+            code: {
+              color: theme("colors.indigo.500"),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme("colors.primary.500"),
+              "&:hover": {
+                color: `${theme("colors.primary.400")}`,
+              },
+              code: { color: theme("colors.primary.400") },
+            },
+            "h1,h2,h3,h4,h5,h6": {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
